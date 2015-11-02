3e71eaf20166d707b25cf393ad63b9e02c713460
memcoy <memccoy@umich.edu>
2015-11-02 13:15:08 -0500

    alarm clock code added but alarm clock screen not added

--- ScavengerHunt/ScavengerHunt/AlarmClockViewController.swift
+++ ScavengerHunt/ScavengerHunt/AlarmClockViewController.swift
@@ -11,15 +11,53 @@ import UIKit
 
 class AlarmClockViewController : UIViewController {
     
-    @IBOutlet var dateTimePicker : UIDatePicker?;
-  
-    func alarmSetButtonTapped(sender:UIButton) {
-        NSLog("Alarm set button tapped");
+    @IBOutlet var dateTimePicker : UIDatePicker!;
+    
+    @IBAction func alarmSetButtonTapped(sender:UIButton) {
+        
+        let dateFormatter = NSDateFormatter();
+        
+        dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle;
+        dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle;
+        
+        let dateTimeString = dateFormatter.stringFromDate(dateTimePicker.date)
+        NSLog("here");
+        NSLog("Alarm set button tapped : %@", dateTimeString);
+        scheduleLocalNotificationWithDate(dateTimePicker.date);
+        
     }
     
-    func alarmCancelButtonTapped(sender:UIButton) {
+    @IBAction func alarmCancelButtonTapped(sender:UIButton) {
         NSLog("Alarm cancel button tapped");
     }
+    
+    func scheduleLocalNotificationWithDate(dateTime : NSDate) {
+        let dateFormatter = NSDateFormatter();
+        let notification:UILocalNotification = UILocalNotification();
+        
+        //var cal = NSCalendar.currentCalendar();
+        
+        notification.fireDate = NSDate(timeIntervalSinceNow: 8)
+        notification.alertBody = "Time to wake up!";
+        notification.soundName = "12_clock.mp3";
+        notification.timeZone = NSCalendar.currentCalendar().timeZone
+        
+        /* Action settings */
+        notification.hasAction = true
+        notification.alertAction = "Open"
+        
+        let fireDateString = dateFormatter.stringFromDate(notification.fireDate!)
+        let dateTimeString = dateFormatter.stringFromDate(dateTime)
+        
+        NSLog("Alarm date time : %@", dateTimeString);
+        NSLog("Alarm fire date : %@", fireDateString);
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(notification);
+        
+    }
+    
+    
+    
 }
 
 
